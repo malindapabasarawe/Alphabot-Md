b1ac8dcdbd46fab4300abe79eefb9bb315cad818
malindapabasarawe <130231646+malindapabasarawe@users.noreply.github.com>
2023-04-21 14:26:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,8 +31,8 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
+global.botname = "pabasara-Md" //namabot kalian
+global.ownername = "pabasara" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
@@ -41,7 +41,7 @@ global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
 global.premium = ['94764497078'] //premium user
 global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
+global.ownernomer = "76447031☆" // nomor wa kalian
 global.ownernomerr = "+94764497078" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
